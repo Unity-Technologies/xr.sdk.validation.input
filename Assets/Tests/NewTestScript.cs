21b7de2c33e3e4e6107a9a0e41688e4386883363
JackPr <jackpr@unity3d.com>
2019-04-10 17:39:35 -0700

    Initial commit

---
+++ Assets/Tests/NewTestScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+using UnityEngine.SceneManagement;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+        [UnityTest]
+        //[Timeout(1000)] // timeout in milliseconds
+        [Description("This test should load the scene, print a debug statement, and succeed")]
+        public IEnumerator TestScene()
+        {
+            string SceneName = "TestScene";
+
+            SceneManager.LoadScene(SceneName);
+            yield return null;
+
+            TestFacilitator facilitator = GameObject.FindGameObjectWithTag("Facilitator").GetComponent<TestFacilitator>();
+            yield return facilitator.RunAllTests();
+            Debug.Log(facilitator.GetTestStatus());
+        }
+
+        [UnityTest]
+        //[Timeout(1000)] // timeout in milliseconds
+        [Description("This test should load the scene, print a debug statement, and succeed")]
+        public IEnumerator TestScene2()
+        {
+            string SceneName = "TestScene";
+
+            SceneManager.LoadScene(SceneName);
+            yield return null;
+
+            TestFacilitator facilitator = GameObject.FindGameObjectWithTag("Facilitator").GetComponent<TestFacilitator>();
+            yield return facilitator.RunAllTests();
+            Debug.Log(facilitator.GetTestStatus());
+        }
+    }
+}
