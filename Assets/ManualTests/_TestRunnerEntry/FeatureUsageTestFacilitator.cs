81653f529b9b43d0777194f34dc898e831d5172f
Jack Pritz <jackpr@unity3d.com>
2019-04-19 11:26:01 -0700

    small changes to FeatureUsage test scene.

---
+++ Assets/ManualTests/_TestRunnerEntry/FeatureUsageTestFacilitator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeatureUsageTestFacilitator : TestFacilitator
+{
+    // This is called by TestRunner scripts.
+    // Simply entering playmode won't start this, and we don't want it to start twice via Start() or Awake()
+    // Please refer to the readme in this project's root folder for more information
+    public override IEnumerator RunTest()
+    {
+        instructionCanvas.Instructions.text = "Activate one of the control buttons below by pressing the corresponding keyboard key or looking directly at it.";
+
+        // This is an example of a manual checkpoint.
+        yield return WaitForContinue();
+
+        // If your test successfully reaches its conclusion, RecordStatus() as passed.
+        // The second parameter of RecordStatus allows you to give more information 
+        // for a success or failure in the results log.
+        if (overallStatus == OverallTestStatus.NotRun)
+            RecordStatus(OverallTestStatus.Passed, "Continue was successfully triggered");
+    }
+}
