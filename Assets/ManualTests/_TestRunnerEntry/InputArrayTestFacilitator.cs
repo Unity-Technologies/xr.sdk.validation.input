2d6caaa2cafaf3fbd975140c16dccc65440729a2
JackPr <jackpr@unity3d.com>
2019-06-05 15:44:16 -0700

    starting to work on the InputArrayTestFacilitator

---
+++ Assets/ManualTests/_TestRunnerEntry/InputArrayTestFacilitator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputArrayTestFacilitator : TestFacilitator
+{
+    // This is called by TestRunner scripts.
+    // Simply entering playmode won't start this, and we don't want it to start twice via Start() or Awake()
+    // Please refer to the readme in this project's root folder for more information
+    public override IEnumerator RunTest()
+    {
+        instructionCanvas.Instructions.text = "For each control on each device, verify that each physical control changes only its expected usage(s). Activate \"Continue\" to cycle through devices.";
+
+        yield return WaitForContinue();
+
+        yield return WaitForContinue();
+
+        // If your test successfully reaches its conclusion, RecordStatus() as passed.
+        // The second parameter of RecordStatus allows you to give more information 
+        // for a success or failure in the results log.
+        if (overallStatus == OverallTestStatus.NotRun)
+            RecordStatus(OverallTestStatus.Passed, "All haptic tests have been manually approved");
+    }
+}
