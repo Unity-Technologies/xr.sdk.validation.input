076fbb5f7a53b004b3348c2c4e2c290b30b20078
JackPr <jackpr@unity3d.com>
2019-06-12 11:21:24 -0700

    Bringing the branch back to 2019.2.0b5, since we are going to release packages into preveiw in 2019.2

--- Assets/ManualTests/InputSubsystem/NameManfSerial/ListDevices.cs
+++ Assets/ManualTests/InputSubsystem/NameManfSerial/ListDevices.cs
@@ -41,14 +41,22 @@ public class ListDevices : MonoBehaviour
 
         InputDevices.GetDevices(devices);
 
+#if UNITY_2019_3_OR_NEWER
         text.text = "";
+#else
+        text.text = "Manufacturer and Serial Number are only accessibly in 2019.3+. You only need to check the names in this Unity version.";
+#endif
 
         for (int i = 0; i < devices.Count; i++)
         {
             if (i != 0)
                 text.text += "\n";
 
+#if UNITY_2019_3_OR_NEWER
             text.text += "[" + (i + 1) + "] Name: " + devices[i].name + ", Manufacturer: " + devices[i].manufacturer + ", Serial Number: " + devices[i].serialNumber;
+#else
+            text.text += "[" + (i + 1) + "] Name: " + devices[i].name + ", ";
+#endif
         }
     }
 }
