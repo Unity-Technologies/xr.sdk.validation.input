7e41b9625ed9205e4c01ab343728ec80cdcff5d6
JackPr <jackpr@unity3d.com>
2019-06-12 17:58:53 -0700

    Adding vector3 magnitude display optional

--- Assets/ManualTests/InputSubsystem/_Shared/FeatureUsageDrivesUI/Vector3FeatureUsageDrivesUI.cs
+++ Assets/ManualTests/InputSubsystem/_Shared/FeatureUsageDrivesUI/Vector3FeatureUsageDrivesUI.cs
@@ -8,6 +8,7 @@ using UnityEngine.UI;
 public class Vector3FeatureUsageDrivesUI : MonoBehaviour
 {
     public Text DisplayText;
+    public bool ShowMagnitude = true;
 
     private InputDevice Device;
     private InputFeatureUsage<Vector3> FeatureUsage;
@@ -27,8 +28,9 @@ public class Vector3FeatureUsageDrivesUI : MonoBehaviour
 
         if (!Device.isValid || !Device.TryGetFeatureValue(FeatureUsage, out State))
             return;
-        
-        DisplayText.text = State.ToString(m_FloatFormatString) +
-            "\nMagnitude = " + State.magnitude.ToString(m_FloatFormatString);
+
+        DisplayText.text = State.ToString(m_FloatFormatString);
+        if (ShowMagnitude)
+            DisplayText.text += "\nMagnitude = " + State.magnitude.ToString(m_FloatFormatString);
     }
 }
