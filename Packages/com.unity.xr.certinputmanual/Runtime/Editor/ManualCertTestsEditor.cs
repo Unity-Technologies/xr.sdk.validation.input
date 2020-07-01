9bf4f327948badcd610928f144b23cf19c3f1cfc
Jack Pritz <jackpr@unity3d.com>
2020-07-01 11:46:55 -0700

    Add editor window to configure runtime wait timer.

---
+++ Packages/com.unity.xr.certinputmanual/Runtime/Editor/ManualCertTestsEditor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ManualCertTestsEditor : EditorWindow
+{
+    internal const string k_WindowTitle = "Manual Certification Configuration";
+    float m_DelaySeconds = -1;
+
+    /// <summary>
+    /// Create a window if necessary, or focus a currently open window.
+    /// The MenuItem attribute inserts this as a function in the editor menu bars.
+    /// </summary>
+    [MenuItem("Tools/" + k_WindowTitle)]
+    public static void ShowWindow()
+    {
+        GetWindow<ManualCertTestsEditor>(k_WindowTitle);
+    }
+
+    /// <summary>
+    /// Specify what is drawn inside the editor window
+    /// </summary>
+    void OnGUI()
+    {
+        if (m_DelaySeconds < 0 && PlayerPrefs.HasKey(ManualCertTestConfig.k_DelaySecondsKey))
+            m_DelaySeconds = PlayerPrefs.GetFloat(ManualCertTestConfig.k_DelaySecondsKey);
+        else
+        {
+            m_DelaySeconds = 0;
+            PlayerPrefs.SetFloat(ManualCertTestConfig.k_DelaySecondsKey, m_DelaySeconds);
+        }
+
+        float NewDelaySeconds = EditorGUILayout.DelayedFloatField(
+            new GUIContent("Runtime setup delay", "The time in seconds to wait while the runtime under test completes setup."), 
+            m_DelaySeconds);
+
+        if (NewDelaySeconds != m_DelaySeconds)
+        {
+            m_DelaySeconds = NewDelaySeconds;
+            PlayerPrefs.SetFloat(ManualCertTestConfig.k_DelaySecondsKey, m_DelaySeconds);
+        }
+    }
+}
---
+++ Packages/com.unity.xr.certinputmanual/Runtime/ManualCertTestConfig.cs
@@ -0,0 +1,5 @@
+﻿public static class ManualCertTestConfig
+{
+    // Time to wait for a runtime to finish setup before running a test.
+    public const string k_DelaySecondsKey = "RuntimeSetupDelay";
+}
